18318a5ae7c6ef14ddf0f73fdd8fcbf199be223a
Prashant G <prashant.gaikwad@leftrightmind.com>
2018-11-24 14:16:33 +0530

    Initial Commit

---
+++ Nav Search Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nav Search Controller
+//
+//  Created by Prashant G on 11/24/18.
+//  Copyright © 2018 MyOrg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
