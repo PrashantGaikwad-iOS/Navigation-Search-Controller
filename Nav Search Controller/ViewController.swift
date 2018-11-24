3db036f47d758ae7024561942a8ce590ed09994d
Prashant G <prashant.gaikwad@leftrightmind.com>
2018-11-24 14:45:44 +0530

    Final Commit

--- Nav Search Controller/ViewController.swift
+++ Nav Search Controller/ViewController.swift
@@ -9,12 +9,117 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
 
+    // MARK: - Properties
+    var candies = [Candy]()
+    let searchController = UISearchController(searchResultsController: nil)
+    var filteredCandies = [Candy]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        candies = [
+            Candy(category:"Chocolate", name:"Chocolate Bar"),
+            Candy(category:"Chocolate", name:"Chocolate Chip"),
+            Candy(category:"Chocolate", name:"Dark Chocolate"),
+            Candy(category:"Hard", name:"Lollipop"),
+            Candy(category:"Hard", name:"Candy Cane"),
+            Candy(category:"Hard", name:"Jaw Breaker"),
+            Candy(category:"Other", name:"Caramel"),
+            Candy(category:"Other", name:"Sour Chew"),
+            Candy(category:"Other", name:"Gummi Bear"),
+            Candy(category:"Other", name:"Candy Floss"),
+            Candy(category:"Chocolate", name:"Chocolate Coin"),
+            Candy(category:"Chocolate", name:"Chocolate Egg"),
+            Candy(category:"Other", name:"Jelly Beans"),
+            Candy(category:"Other", name:"Liquorice")
+        ]
+        
+        setupNavBar()
     }
-
-
+    
+    func setupNavBar() {
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        searchController.searchBar.searchBarStyle = .minimal
+        // Setup the Search Controller
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search Candies"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
+        
+        // navigationItem.hidesSearchBarWhenScrolling = false
+    }
+    
+    // MARK: - Private instance methods
+    func isFiltering() -> Bool {
+        return searchController.isActive && !searchBarIsEmpty()
+    }
+    
+    func searchBarIsEmpty() -> Bool {
+        // Returns true if the text is empty or nil
+        return searchController.searchBar.text?.isEmpty ?? true
+    }
+    
+    func filterContentForSearchText(_ searchText: String, scope: String = "All") {
+        filteredCandies = candies.filter({( candy : Candy) -> Bool in
+            return candy.name.lowercased().contains(searchText.lowercased())
+        })
+        tableView.reloadData()
+    }
+    
 }
 
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    // MARK: - TableView Methods
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if isFiltering() {
+            return filteredCandies.count
+        }
+        return candies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CellID", for: indexPath)
+        let candy: Candy
+        if isFiltering() {
+            candy = filteredCandies[indexPath.row]
+        } else {
+            candy = candies[indexPath.row]
+        }
+        cell.textLabel!.text = candy.name
+        cell.detailTextLabel!.text = candy.category
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+}
+
+extension ViewController: UISearchResultsUpdating {
+    // MARK: - UISearchResultsUpdating Delegate
+    func updateSearchResults(for searchController: UISearchController) {
+        // TODO
+        filterContentForSearchText(searchController.searchBar.text!)
+    }
+}
+
+
+struct Candy {
+    let category : String
+    let name : String
+}
